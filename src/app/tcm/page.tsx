fddf01f2235d22fc18a871039f67212a54502746
Zainen Suzuki <zainensuzuki@pop-os.localdomain>
2023-12-21 23:18:10 -0800

    add table formatting. more to do

--- src/app/tcm/page.tsx
+++ src/app/tcm/page.tsx
@@ -4,7 +4,7 @@ import { HerbTable } from "@/components/ui/tcm-table/HerbTable"
 const tcm = () => {
   return (
     <div className="flex justify-center w-full bg-tttt-200">
-      <HerbTable className="flex flex-col justify-center w-full md:w-2/3 text-primary-400" />
+      <HerbTable className="flex flex-col  w-full md:w-4/5 text-primary-400 my-4 border-primary-400 border-2" />
     </div>
   )
 }
--- src/components/ui/tcm-table/HerbTable.tsx
+++ src/components/ui/tcm-table/HerbTable.tsx
@@ -8,17 +8,17 @@ import HerbRow from "./HerbRow";
 export const HerbTable = (props: {className?: string;}) => {
   const {herbIndexes, herbList, isError, isLoading} = useTcmHerbJist();
   return (
-    <table className={props.className}>
-      <thead>
-        <tr className="flex flex-1 justify-between">
-          <th>TCM Name English</th>
-          <th>TCM Name</th>
-          <th>Herb Pinyin Name</th>
-          <th>Herb Latin Name</th>
-          <th>Properties</th>
-          <th>Meridians</th>
-          <th>Therapeutic Class English</th>
-          <th>Therapeutic Class</th>
+    <table className={`table-auto ${props.className}`}>
+      <thead className="border-primary-400 border-b-2">
+        <tr className="flex">
+          <th className="flex px-4 items-center flex-1 border-r-2 py-2">TCM Name English</th>
+          <th className="flex px-4 items-center flex-1 border-r-2 py-2">TCM Name</th>
+          <th className="flex px-4 items-center flex-1 border-r-2 py-2">Herb Pinyin Name</th>
+          <th className="flex px-4 items-center flex-1 border-r-2 py-2">Herb Latin Name</th>
+          <th className="flex px-4 items-center flex-1 border-r-2 py-2">Properties</th>
+          <th className="flex px-4 items-center flex-1 border-r-2 py-2">Meridians</th>
+          <th className="flex px-4 items-center flex-1 border-r-2 py-2">Therapeutic Class English</th>
+          <th className="flex px-4 items-center flex-1">Therapeutic Class</th>
         </tr>
       </thead>
       <tbody>
