70052d6869514485c56bdec65f7419f2f68faa7b
Zainen Suzuki <zainensuzuki@pop-os.localdomain>
2024-01-11 13:29:49 -0800

    clean unneeded, change table css and fix rows spacing

--- src/app/tcm/[pageNumber]/page.tsx
+++ src/app/tcm/[pageNumber]/page.tsx
@@ -1,7 +1,7 @@
 import Pagination from "@/components/ui/Pagination";
-import { HerbTable } from "@/components/ui/tcm-table/HerbTable"
+import { formatSymbol } from "@/helperFunctions";
 import { GetHerbJist, HerbJist } from "@/types";
-import { useRouter } from "next/router";
+import Link from "next/link";
 
 const getHerbs = async (page: string) => {
   const apiCall = await fetch(`${process.env.NEXT_PUBLIC_KAINESS_API}/tcm/${page}`, { 
@@ -32,9 +32,79 @@ export default async function Page (props: Props) {
     console.log("FAILED WITH ", e)
   }
   return (
-    <div className="flex py-4 md:py-8 flex-col items-center w-full bg-tttt-200 min-w-fit">
-      <HerbTable className="text-primary-400 border-primary-400 border-2"  pages={pages} herbs={herbs}/>
-      <div className="w-full mt-4 md:w-4/5 flex justify-center md:justify-end items-center">
+    <div className="flex py-4 md:py-16 flex-col items-center w-full bg-tttt-200 min-w-fit">
+      <div className="flex flex-col">
+        <div className=" overflow-x-auto">
+          <div className="inline-block min-w-full my-2, align-middle md:px-6 lg:px-8">
+            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5">
+              <table className={`table-auto text-primary-400 border-primary-400 border-2 overflow-x-scroll scrollbar-none`}>
+                <thead className="border-primary-400 border-b-2">
+                  <tr>
+                    <th className="p-2 border-r-2">TCM Name English</th>
+                    <th className="hidden md:table-cell p-2 border-r-2">TCM Name</th>
+                    <th className="hidden md:table-cell p-2 border-r-2">Herb Pinyin Name</th>
+                    <th className="hidden lg:table-cell p-2 border-r-2">Herb Latin Name</th>
+                    <th className="hidden md:table-cell p-2 border-r-2">Properties</th>
+                    <th className="p-2 border-r-2">Meridians</th>
+                    <th className="p-2 border-r-2">Therapeutic Class English</th>
+                    <th className="hidden md:table-cell px-2">Therapeutic Class</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {herbs && herbs.length ? 
+                    <>
+                      {herbs.map((herb, i) => {
+                        return (
+                          <tr key={`herb_row_key_${i}`} className="border-b-[1px]">
+                            <td className={`border-primary-400 border-r-[1px] py-1 px-2 ${herb.tcm_name_en ?? "bg-gray-700/15"}`}>
+                              <Link href={`/tcm/herb/${herb.id}`} className="underline">
+                                <div className=" flex flex-col">
+                                  {
+                                    formatSymbol(herb.tcm_name_en ?? '', ';').map((str, i) => {
+                                      return <p key={`english_name_${i}`}>{str}</p>
+                                    })
+                                  }
+                              </div>
+                              </Link>
+                            </td>
+                            <td className={`hidden md:table-cell border-primary-400 border-r-[1px] py-1 px-2 ${herb.tcm_name ?? "bg-gray-700/15 "}`}>{herb.tcm_name ?? ""}</td>
+                            <td className={`hidden md:table-cell border-primary-400 border-r-[1px] py-1 px-2 ${herb.herb_pinyin_name ?? "bg-gray-700/15"}`}>{herb.herb_pinyin_name ?? ""}</td>
+                            <td className={`hidden lg:table-cell border-primary-400 border-r-[1px] py-1 px-2 ${herb.herb_latin_name ?? "bg-gray-700/15"}`}>{herb.herb_latin_name ?? ""}</td>
+                            <td className={`hidden md:table-cell border-primary-400 border-r-[1px] py-1 px-2 ${herb.properties ?? "bg-gray-700/15"}`}>
+                              <div className="flex flex-col">
+                                {
+                                  formatSymbol(herb.properties ?? '', '; ').map((str, i) => {
+                                    return <p key={`property_${i}`}>{str}</p>
+                                  })
+                                }
+                              </div>
+                            </td>
+                            <td className={`border-primary-400 border-r-[1px] py-1 px-2 ${herb.meridians ?? "bg-gray-700/15"}`}>
+                              <div className="flex flex-col">
+                                {
+                                  formatSymbol(herb.meridians ?? '', '; ').map((str, i) => {
+                                    return <p key={`meridian_${i}`}>{str}</p>
+                                  })
+                                }
+                              </div>
+                            </td>
+                            <td className={`border-primary-400 border-r-[1px] py-1 px-2 ${herb.therapeutic_en_class ?? "bg-gray-700/15"}`}>{herb.therapeutic_en_class ?? ""}</td>
+                            <td className={`hidden md:table-cell py-1 px-2 ${herb.therapeutic_cn_class ?? "bg-gray-700/15"}`}>{herb.therapeutic_cn_class ?? ""}</td>
+                          </tr>
+                        )
+                      })}
+                    </>
+                    : 
+                    <></>
+                  }
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full mt-4 md:pr-16 flex justify-center md:justify-end items-center">
+        {/* TODO possibly change pages to next and prev button with a go to page input */}
         <Pagination totalPages={pages.length} currentPage={Number(props.params.pageNumber)}/>
       </div>
     </div>
--- src/components/ui/Pagination.tsx
+++ src/components/ui/Pagination.tsx
@@ -14,10 +14,11 @@ export default function Pagination({ totalPages, currentPage }: { totalPages: nu
 
   const high = plusFive > totalPages ? totalPages : plusFive;
 
-  const pages = [];
+  const pages: number[] = [];
   for (let i = low; i <= high; i++) {
     pages.push(i);
   }
+  const dropPageOptionsIfIncludes = [0,  1 ,  pages.length - 1,  pages.length - 2];
 
   const [includesFirst, isNotNext] = [pages.includes(1), !pages.includes(2)];
   const [includesLast, isNotLast] = [pages.includes(totalPages), !pages.includes(totalPages-1)];
@@ -35,7 +36,7 @@ export default function Pagination({ totalPages, currentPage }: { totalPages: nu
         pages.map((page, i) => {
           return (
             <li key={`page_number_${page}`} className="h-full">
-              <Link href={`/tcm/${page}`} className={`px-3 py-2 border-r ${currentPage === page ? "bg-primary-100" : "hover:bg-primary-200"}`}>{page}</Link>
+              <Link href={`/tcm/${page}`} className={`${dropPageOptionsIfIncludes.includes(i) ? "hidden md:block" : ""} px-3 py-2 border-r ${currentPage === page ? "bg-primary-100" : "hover:bg-primary-200"}`}>{page}</Link>
             </li>
           )
         })
